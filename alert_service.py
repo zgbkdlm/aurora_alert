6e23d02d6cce381602dee7450de356ee0d9d4d3d
George Z <zgbkdlm@gmail.com>
2018-01-01 12:00:14 +0200

    Update alert_service.py

--- alert_service.py
+++ alert_service.py
@@ -1,8 +1,12 @@
 import aurora_spider
+import push_api
 
 aSpider = Aurora_spider()
 
 # Updating the forecast in cycle
 forecast = aSpider.sniff_short()
 
-# Call alert api, for example SMS, email, Telegram Bot etc.
+# Check if need to send notification
+kp_threshold = 6
+
+# Call push api, so that the notification can be pushed to users.
