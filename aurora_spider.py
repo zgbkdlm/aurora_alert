e054a2864f6e4e7cad2a649b4bb663f7d6fe509b
George Z <zgbkdlm@gmail.com>
2018-01-01 11:51:29 +0200

    Update aurora_spider.py

--- aurora_spider.py
+++ aurora_spider.py
@@ -15,12 +15,11 @@ class Aurora_spider:
         
         kp_value = soup.find_all(style="color: #0000ff;")
         p=r'\w\.\w\w$'
-        
-        return [re.findall(p, kp_value[0].text)[0] \
-                kp_value[1].text re.findall(p, kp_value[2].text)[0] \
-                kp_value[3].text re.findall(p, kp_value[4].text)[0] \
-                kp_value[5].text re.findall(p, kp_value[6].text)[0]]
-        
-        
+
+        return [re.findall(p, kp_value[0].text)[0],
+            kp_value[1].text, re.findall(p, kp_value[2].text)[0],
+            kp_value[3].text, re.findall(p, kp_value[4].text)[0],
+            kp_value[5].text, re.findall(p, kp_value[6].text)[0]]
+
     def sniff_long(self):
-        a=1
+        
