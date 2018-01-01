99b9c7c4a7c9e2cbf3950fd1395c1da2c4a04618
George Z <zgbkdlm@gmail.com>
2018-01-01 11:57:44 +0200

    Update alert_service.py

--- alert_service.py
+++ alert_service.py
@@ -1,5 +1,8 @@
-from aurora_spider import Aurora_spider
+import aurora_spider
 
 aSpider = Aurora_spider()
 
+# Updating the forecast in cycle
 forecast = aSpider.sniff_short()
+
+# Call alert api, for example SMS, email, Telegram Bot etc.
