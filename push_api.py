b7801247320a2c4bcfbdf71c73a558add30a2e5d
George Z <zgbkdlm@gmail.com>
2018-01-01 12:36:56 +0200

    Update push_api.py

--- push_api.py
+++ push_api.py
@@ -1,8 +1,12 @@
 import push_config
+import requests
 
 class Push_api():
     
     def __init__(self):
         
-    def tele_push(self):
-        
+    def tele_push(self, content):
+        url = "https://api.telegram.org/bot" + bot_token + "/"
+        r = request.get(url + getUpdates)
+        r = requests.post(url + "sendMessage", 
+                          data={'chat_id':chat_id, 'text':'ttttttt'})
