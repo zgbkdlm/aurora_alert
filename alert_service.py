4a7f23a48399e73fc6a6ca12f917c590fd840642
George Z <zgbkdlm@gmail.com>
2018-01-01 12:09:46 +0200

    Update alert_service.py

--- alert_service.py
+++ alert_service.py
@@ -2,11 +2,13 @@ import aurora_spider
 import push_api
 
 aSpider = Aurora_spider()
+aPush = Push_api()
 
-# Updating the forecast in cycle
+# Updating the forecast in cycle, Crontab is recommended.
 forecast = aSpider.sniff_short()
 
 # Check if need to send notification
 kp_threshold = 6
 
 # Call push api, so that the notification can be pushed to users.
+aPush.tele_push(forecast)
